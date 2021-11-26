b85b7d50a70e690250a48114c6bc5dcd52fd1fbf
RoeJecc <77800695+RoeJecc@users.noreply.github.com>
2021-11-26 07:49:57 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/around-react">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
