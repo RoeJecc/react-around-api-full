80a2d6c794935397aa1b466cd595ca326ef7157f
RoeJecc <77800695+RoeJecc@users.noreply.github.com>
2021-12-02 14:39:01 -0500

    environment fixing hopefully

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
-
+const dotenv = require('dotenv');
 const AuthenticationError = require('../errors/authentication-error');
 
-
+dotenv.config();
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
@@ -18,7 +18,7 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(
       token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'super-secret-key'
+      NODE_ENV === 'production'
     );
   } catch (err) {
     throw new AuthenticationError('Authentication Required.')
