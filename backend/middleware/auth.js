dc5e2712f0b7e9f1548dd2c0a6ab681bf9a34f8e
RoeJecc <77800695+RoeJecc@users.noreply.github.com>
2021-12-02 14:43:32 -0500

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const AuthenticationError = require('../errors/authentication-error');
 
 dotenv.config();
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
