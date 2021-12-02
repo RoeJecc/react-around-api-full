b3799dce6971dc638c0b6d5abf1370a1afbd21ee
RoeJecc <77800695+RoeJecc@users.noreply.github.com>
2021-12-02 15:33:31 -0500

    user controller tweak

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,50 +8,50 @@ const AuthenticationError = require("../errors/authentication-error");
 const ConflictError = require("../errors/conflict-error");
 const User = require("../models/user");
 
+dotenv.config();
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 function getUsers(req, res, next) {
   User.find({})
-    .then((users) => res.status(200).send({ data: users }))
+    .select("+password")
+    .then((users) => res.send({ data: users }))
     .catch(next);
 }
 
 function getUserById(req, res, next) {
-  User.findById(req.params.id)
+  User.findById(req.params.id === "me" ? req.user._id : req.params.id)
+    .select("+password")
     .then((user) => {
-      if (!user) {
-        throw new NotFoundError("User not found.");
+      if (user) {
+        res.send({ data: user });
       } else {
-        return res.status(200).send({ user });
+        throw new NotFoundError("User not found.");
       }
     })
     .catch(next);
 }
 
 const getCurrentUser = (req, res, next) => {
-  User.findById(req.user._id)
+  User.findById(req.params.id === "me" ? req.user._id : req.params.id)
+    .select("+password")
     .then((user) => {
-      if (!user) {
-        throw new NotFoundError("User not found");
+      if (user) {
+        res.send({ user });
       } else {
-        return res.status(200).send({ user });
+        throw new NotFoundError("User not found.");
       }
+      next(err);
     })
-    .catch(console.log(req), next);
+    .catch(next);
 };
 
 function createUser(req, res, next) {
-  const { name, about, avatar, email, password } = req.body;
-
-  User.findOne({ email }).then((userExists) => {
-    if (userExists) {
-      throw new ConflictError("User already exists.");
-    }
-  });
-
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar, email, password });
   bcrypt
     .hash(password, 10)
-    .then((hash) => User.create({
+    .then((hash) =>
+      User.create({
         name,
         about,
         avatar,
@@ -59,13 +59,14 @@ function createUser(req, res, next) {
         password: hash,
       })
     )
-    .then((user) => res.status(200).send({ _id: user._id, email: user.email }))
+    .then((user) => res.status(201).send({ _id: user._id }))
     .catch((err) => {
-      if (err.name === "MongoError" && err.code === 11000) {
-        throw new BadRequestError("User already exists.");
-      } else {
-        next(err);
+      if (err.name === "ValidationError") {
+        throw new BadRequestError("Unable to create user.");
+      } else if (err.name === "MongoError") {
+        throw new ConflictError("User already exists.");
       }
+      next(err);
     })
     .catch(next);
 }
@@ -73,54 +74,63 @@ function createUser(req, res, next) {
 function updateUser(req, res, next) {
   const { name, about } = req.body;
   User.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
+    { _id: req.user._id },
+    { $set: { name, about } },
     { new: true, runValidators: true }
   )
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError("User not found.");
-      } else {
-        res.status(200).send({ data: user });
+    .select("+password")
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        throw new BadRequestError("Unable to update user.");
       }
     })
     .catch(next);
 }
 
 function updateAvatar(req, res, next) {
-  const { avatar } = req.body;
-
   User.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
+    { _id: req.user._id },
+    { avatar: req.body.avatar },
     { new: true, runValidators: true }
   )
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError("User not found.");
-      } else {
-        res.status(200).send({ data: user });
+    .select("+password")
+    .then((user) => res.send({ data: user}))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new BadRequestError('Unable to update avatar.');
       }
+      next(err);
     })
     .catch(next);
 }
 
 function login(req, res, next) {
   const { password, email } = req.body;
-
-  return User.findUserByCredentials(password, email)
-    .then((user) => {
-      if (!user) {
-        throw new AuthorizationError("Not Authorized");
-      }
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === "production",
-        { expiresIn: "7d" }
-      );
-      res.send({ token });
-    })
-    .catch(next);
+  User.findOne({ email })
+  .select('+password')
+  .then((user) => {
+    if (!user) {
+      throw new AuthenticationError('Incorrect email or password.');
+    } else {
+      req._id = user._id;
+      return bcrypt.compare(password, user.password);
+    }
+  })
+  .then((matched) => {
+    if (!matched) {
+      throw new AuthenticationError('Incorrect email or password.');
+    }
+    const token = jwt.sign(
+      { _id: req._id },
+      NODE_ENV === 'production',
+      { expiresIn: '7d' }
+    );
+    res.header('authorization', `Bearer ${token}`);
+    res.cookie('token', token, { httpOnly: true });
+    res.status(200).send({ token });
+  })
+  .catch(next);
 }
 
 module.exports = {
